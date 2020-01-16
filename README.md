e62d4f2fe8fd444785ccf08ded8828336249d46d
Isak Johansson <isak@k22.se>
2020-01-16 12:58:50 +0100

    Cleaned up the code a bit and added the cli part.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tablex
+
+A cli tool to generate tables(currently only ascci tables) from regex capture groups.
+
+## Usage
+```
+tablex 0.0.1
+Isak Johansson
+
+USAGE:
+    tablex [ARGS]
+    tablex [ARGS] <SUBCOMMAND>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <regex>         
+    <input-file>    
+
+SUBCOMMANDS:
+    help    Prints this message or the help of the given subcommand(s)
+    view    
+```
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use lazy_static::lazy_static;
-use prettytable::{cell, Cell, row, Row, Table, table};
-use regex::Regex;
+use prettytable::{cell, row, Cell, Row, Table};
+use rayon::prelude::*; use regex::{Captures, Regex};
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::fs::read_to_string;
+use std::env;
+use std::path::{PathBuf, Path};
+use structopt::{clap::AppSettings, StructOpt};
 
 type Result<T> = std::result::Result<T, std::io::Error>;
 
@@ -13,9 +15,33 @@ struct Config {
 }
 #[derive(Deserialize, Serialize, Debug)]
 struct View {
-    regexes: Vec<String>,
+    regex: String,
     file: String,
-    labels: Vec<String>
+}
+impl Config {
+    pub fn from(path: &Path) -> Result<Self> {
+        let config_str = read_to_string(path)?;
+        Ok(toml::from_str(&config_str)?)
+    }
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(setting = AppSettings::SubcommandsNegateReqs, author)]
+enum CliSubCommand {
+    View {
+        name: String
+    }
+}
+
+#[derive(Debug, StructOpt)]
+struct Cli {
+    regex: Option<String>,
+
+    input_file: Option<PathBuf>,
+    
+    #[structopt(subcommand)]
+    cmd: Option<CliSubCommand>
+
 }
 
 fn main() {
@@ -25,50 +51,96 @@ fn main() {
     }
 }
 
+fn get_labels(re: &Regex) -> Vec<String> {
+    re.capture_names()
+        .into_iter()
+        .flatten()
+        .map(|s| s.to_string())
+        .collect()
+}
+fn get_named<'a>(cap: &'a Captures, label: &str) -> &'a str {
+    &cap.name(label).map_or("", |m| m.as_str())
+}
+
+fn get_config_path() -> PathBuf {
+    let base_dir_str = match env::var("XDG_CONFIG_HOME") {
+        Ok(path) => path,
+        Err(_) => env::var("HOME").expect("No environment variable set for $HOME")
+    };
+    PathBuf::from(base_dir_str).join("tablex/config.toml")
+}
+struct RegexTable {
+    re: Regex,
+    title: String,
+    labels: Vec<String>,
+}
+
+impl RegexTable {
+    fn new(regex_str: &str, title: String) -> Self {
+        let re: Regex = Regex::new(regex_str).expect("regex does not compile!");
+        Self {
+            labels: get_labels(&re),
+            re,
+            title,
+        }
+    }
+    fn ascii(&self, content: String) {
+        let mut tbl = Table::new();
+
+        tbl.add_row(row![self.title]);
+        tbl.add_row(Row::new(
+            self.labels
+                .iter()
+                .map(|l| Cell::new(l))
+                .collect::<Vec<Cell>>(),
+        ));
+
+        let rows: Vec<Row> = content.par_lines().map(|line| self.re.captures(line) )
+            .flatten()
+            .map(|cap|
+                    Row::new(self.labels
+                        .iter()
+                        .map(|label| Cell::new(get_named(&cap, &label)))
+                        .collect::<Vec<Cell>>())
+            ).collect();
+
+        for row in rows {
+            tbl.add_row(row);
+        };
+
+        tbl.printstd();
+    }
+}
 
 fn try_main() -> Result<()> {
-    use std::collections::BTreeMap;
+    let config_path = get_config_path();
+    let config = Config::from(&config_path)?;
 
-    let config_str = read_to_string("./config.toml")?;
-    let config: Config = toml::from_str(&config_str)?;
-    println!("{:#?}", &config);
+    let cli = Cli::from_args();
 
 
-    for (view_name, view_config) in &config.view {
-        let content = read_to_string(&view_config.file)?;
+    if let Some(subcmd) = cli.cmd {
+        match subcmd {
+            CliSubCommand::View {
+                name,
+            } => {
+                let view = config.view.get(&name).expect("ERROR: No views by that name found");
+                
+                let content = read_to_string(&view.file)?;
+                let tbl = RegexTable::new(&view.regex, name.clone());
 
-        let mut regex_vec: Vec<Regex> = Vec::new();
-        for regex_str in &view_config.regexes {
-            let regex: Regex = Regex::new(regex_str).expect("regex does not compile!");
-            regex_vec.push(regex);
+                tbl.ascii(content);
+                
+            }
         };
+    } else {
+        if let (Some(regex), Some(file)) = (&cli.regex, &cli.input_file) {
+                let content = read_to_string(file)?;
+                let tbl = RegexTable::new(regex, "".to_string());
 
-        let mut output_table = Table::new();
-        output_table.add_row(row![view_name]);
-        let mut label_row: Vec<Cell> = Vec::new();
-        for label in &view_config.labels {
-            label_row.push(Cell::new(&label))
-        };
-        output_table.add_row(Row::new(label_row));
-
-        for line in content.lines() {
-            //println!("{:#?}", &line);
-            for regex in &regex_vec {
-                if let Some(cap) = regex.captures(line) {
-                    //println!("{:#?}", cap);
-                    let mut tbl_row: Vec<Cell> = Vec::new();
-
-                    for label in &view_config.labels {
-                        let val = &cap.name(label).map_or("", |m | m.as_str());
-                        tbl_row.push(Cell::new(val))
-                    };
-                    output_table.add_row(Row::new(tbl_row));
-                };
-
-            };
-        };
-        output_table.printstd();
+                tbl.ascii(content);
+        }
+        
     };
-
     Ok(())
 }
