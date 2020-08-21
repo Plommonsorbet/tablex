cacf8fc7dbdc9df797b6055e7aabf94eb897c157
Isak Johansson <isak@k22.se>
2020-08-21 10:11:40 +0200

    UPDATE nix-shell to include extensions

--- shell.nix
+++ shell.nix
@@ -1,17 +1,42 @@
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  moz_overlay = import (builtins.fetchTarball
+    "https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz");
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "moz_overlay_shell";
-    buildInputs = [
-      # to use the latest nightly:
-      nixpkgs.latest.rustChannels.nightly.rust
-      # to use a specific nighly:
-      (nixpkgs.rustChannelOf { channel = "nightly"; }).rust
-      # to use the project's rust-toolchain file:
-      #(nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
-    ];
 
-  } 
+  rust_nightly = (nixpkgs.latest.rustChannels.nightly.rust.override {
+    extensions = [ "rust-src" "rust-analysis" ]
+
+    ;
+  });
+in with nixpkgs;
+
+stdenv.mkDerivation {
+  name = "rust-nightly-dev";
+  buildInputs = [
+    rust_nightly
+    rustup
+    # to use the latest nightly:
+    #nixpkgs.latest.rustChannels.nightly.rust 
+
+    # to use a specific nighly:
+    #(nixpkgs.rustChannelOf { channel = "nightly"; }).rust
+    # to use the project's rust-toolchain file:
+    #(nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
+    #(nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
+  ];
+
+}
+#let
+#  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+#  nixpkgs = import <nixpkgs> {
+#    overlays = [ moz_overlay ];
+#  };
+#  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override {
+#    extensions = [ "rust-src" "rust-analysis" ];}
+#  );
+#in
+#  with nixpkgs;
+#  stdenv.mkDerivation {
+#    name = "rust";
+#    buildInputs = [ rustup ruststable ];
+#  }
